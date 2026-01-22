e2f92e976e73352d314d264a98461e68777dae74
Shenara DE SILVA <DELL@DESKTOP-VFSVIGN>
2026-01-22 21:42:25 +0530

    Initial version of NonPerishable.java class file created.

--- src/main/java/com/csp3341/grocery/NonPerishable.java
+++ src/main/java/com/csp3341/grocery/NonPerishable.java
@@ -1,4 +1,28 @@
 package com.csp3341.grocery;
 
-public class NonPerishable {
+public class NonPerishable extends Product {
+    private String shelfLife;
+
+    public NonPerishable(int id, String name, double price, int quantity, Category category, String shelfLife) {
+        super(id, name, price, quantity, category);
+        this.shelfLife = shelfLife;
+    }
+
+    public String getShelfLife() {
+        return shelfLife;
+    }
+
+    public void setShelfLife(String shelfLife) {
+        this.shelfLife = shelfLife;
+    }
+
+    @Override
+    public boolean isExpired() {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " | Shelf Life: " + shelfLife;
+    }
 }
