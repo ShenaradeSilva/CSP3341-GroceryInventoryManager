aaa691b83c37cd4440c201b6a1ec93aa463e0331
Shenara DE SILVA <DELL@DESKTOP-VFSVIGN>
2026-01-22 20:44:27 +0530

    Initial version of Category.java enum file created.

--- src/main/java/com/csp3341/grocery/Category.java
+++ src/main/java/com/csp3341/grocery/Category.java
@@ -1,4 +1,15 @@
 package com.csp3341.grocery;
 
 public enum Category {
+    DAIRY,
+    PRODUCE,
+    MEAT,
+    BEVERAGES,
+    CANNED_FOOD,
+    DRIED_FOOD;
+
+    @Override
+    public String toString() {
+        return name();
+    }
 }
