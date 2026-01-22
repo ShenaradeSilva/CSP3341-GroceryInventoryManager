f82660e72df8f4e494f638690e1dfc224f74fdda
Shenara DE SILVA <DELL@DESKTOP-VFSVIGN>
2026-01-22 21:47:15 +0530

    Initial version of Supplier.java class file created.

--- src/main/java/com/csp3341/grocery/Supplier.java
+++ src/main/java/com/csp3341/grocery/Supplier.java
@@ -1,4 +1,34 @@
 package com.csp3341.grocery;
 
 public class Supplier {
+    private int supplierId;
+    private String supplierName;
+    private String contact;
+
+    public Supplier(int supplierId, String supplierName, String contact) {
+        this.supplierId = supplierId;
+        this.supplierName = supplierName;
+        this.contact = contact;
+    }
+
+    public int getSupplierId() {
+        return supplierId;
+    }
+
+    public String getSupplierName() {
+        return supplierName;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    @Override
+    public String toString() {
+        return supplierId + " | " + supplierName + " | " + contact;
+    }
 }
